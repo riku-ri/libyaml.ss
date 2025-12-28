8e35c2563c6f0072ae8e4dafe45cfd78eb4a737e
luli <riku.ri@outlook.com>
2025-12-29 03:53:47 +0800

    Remove tag level in archive path

--- bin/woodpecker/event/tag/git-archive--with-submodule.sh
+++ bin/woodpecker/event/tag/git-archive--with-submodule.sh
@@ -3,8 +3,8 @@
 set -exuo pipefail
 
 CI_REPO_NAME="${CI_REPO_NAME}" && test -n "${CI_REPO_NAME}"
-CI_COMMIT_TAG="${CI_COMMIT_TAG}" && test -n "${CI_COMMIT_TAG}"
+#CI_COMMIT_TAG="${CI_COMMIT_TAG}" && test -n "${CI_COMMIT_TAG}"
 
 git submodule update --init --recursive
-git submodule foreach sh -c 'git archive HEAD '"--prefix=$CI_REPO_NAME/$CI_COMMIT_TAG/"'$(realpath --relative-to=$(git rev-parse --show-superproject-working-tree) .)/ | tar x -C $(git rev-parse --show-superproject-working-tree)/.local'
-git archive HEAD --prefix="$CI_REPO_NAME/$CI_COMMIT_TAG/" | tar x -C .local
+git submodule foreach sh -c 'git archive HEAD '"--prefix=$CI_REPO_NAME/"'$(realpath --relative-to=$(git rev-parse --show-superproject-working-tree) .)/ | tar x -C $(git rev-parse --show-superproject-working-tree)/.local'
+git archive HEAD --prefix="$CI_REPO_NAME/" | tar x -C .local
