b2528f529e685591d6af072de266f3b75ae8743a
luli <riku.ri@outlook.com>
2025-12-29 03:34:31 +0800

    CI_REPO_NAME but not CI_REPO as tar prefix in release-info action

--- bin/woodpecker/event/tag/git-archive--with-submodule.sh
+++ bin/woodpecker/event/tag/git-archive--with-submodule.sh
@@ -2,9 +2,9 @@
 
 set -exuo pipefail
 
-CI_REPO="${CI_REPO}" && test -n "${CI_REPO}"
+CI_REPO_NAME="${CI_REPO_NAME}" && test -n "${CI_REPO_NAME}"
 CI_COMMIT_TAG="${CI_COMMIT_TAG}" && test -n "${CI_COMMIT_TAG}"
 
 git submodule update --init --recursive
-git submodule foreach sh -c 'git archive HEAD '"--prefix=$CI_REPO/$CI_COMMIT_TAG/"'$(realpath --relative-to=$(git rev-parse --show-superproject-working-tree) .)/ | tar x -C $(git rev-parse --show-superproject-working-tree)/.local'
-git archive HEAD --prefix="$CI_REPO/$CI_COMMIT_TAG/" | tar x -C .local
+git submodule foreach sh -c 'git archive HEAD '"--prefix=$CI_REPO_NAME/$CI_COMMIT_TAG/"'$(realpath --relative-to=$(git rev-parse --show-superproject-working-tree) .)/ | tar x -C $(git rev-parse --show-superproject-working-tree)/.local'
+git archive HEAD --prefix="$CI_REPO_NAME/$CI_COMMIT_TAG/" | tar x -C .local
